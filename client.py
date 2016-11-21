b1b9cf1783b1341ec86f907e92e845ff75277e91
lmillana <leyreeemcaballero@gmail.com>
2016-11-21 17:00:42 +0100

    Cliente PEP8

--- client.py
+++ client.py
@@ -15,15 +15,14 @@ if len(sys.argv) != 3:
 # Dirección IP del servidor:
 try:
     METHOD = sys.argv[1].upper()
-    SERVER = sys.argv[2].split('@')[0]
+    LOGIN = sys.argv[2].split('@')[0]
     IP = sys.argv[2].split('@')[1].split(':')[0]
-    PORT = int(sys.argv[2].split('@')[1].split(':')[1])
+    PORT = int(sys.argv[2].split(':')[1])
 except Exception:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
-
 # Contenido que vamos a enviar:
-LINE = METHOD + ' sip:' + SERVER + '@' + IP + ' SIP/2.0\r\n'
+LINE = METHOD + ' sip:' + LOGIN + '@' + IP + ' SIP/2.0\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,16 +31,20 @@ my_socket.connect((IP, PORT))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+
 try:
     data = my_socket.recv(1024)
 except ConnectionRefusedError:
     sys.exit('Connection refuses')
 
+#Lo que recibo antes del asentimiento.
+print(data.decode('utf-8'))
+
 #Metodo de asentimiento:
 message = data.decode('utf-8').split('\r\n\r\n')[0:-1]
 
 if message == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring', 'SIP/2.0 200 OK']:
-    MESS_ACK = 'ACK sip:' + RECEPTOR + '@' + IP + ' SIP/2.0\r\n'
+    MESS_ACK = 'ACK ' + LOGIN + '@' + IP + ' SIP/2.0\r\n'
     print("Enviando: " + MESS_ACK)
     my_socket.send(bytes(MESS_ACK, 'utf-8') + b'\r\n')
     try:
@@ -49,7 +52,7 @@ if message == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring', 'SIP/2.0 200 OK']:
     except ConnectionRefusedError:
         sys.exit('Connection refuses')
 
-print('Recibido -- ', data.decode('utf-8'))
+print('Recibido -- ', message)
 print("Terminando socket...")
 
 # Cerramos todo
